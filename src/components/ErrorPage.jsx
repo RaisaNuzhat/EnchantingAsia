23ade8eab3b01a4407e2c11832613bc8eb42fe6f
RaisaNuzhat <raisa.csecu@gmail.com>
2024-04-30 15:42:29 +0600

    github login done

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,10 @@
-
-
+import Lottie from "lottie-react";
+import error  from '../assets/error.json'
 const ErrorPage = () => {
     return (
-        <div>
-            <h3 className="text-center my-10 mx-auto container text-red-600">not found</h3>
+        <div className="container mx-auto my-3 flex flex-col justify-center">
+            <h3 className="text-center font-Montserrat font-semibold my-10 "> ooppppppppppssssssssss</h3>
+            <Lottie animationData={error} loop={true} />
         </div>
     );
 };
